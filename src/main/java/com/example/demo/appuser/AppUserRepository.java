25c2f2be4546a4c2481dfbfc8b26c65146e4e310
Łukasz Sławęta <lukasz.slaweta@gmail.com>
2023-07-19 14:43:32 +0200

    Add user roles, registration servise

---
+++ src/main/java/com/example/demo/appuser/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.appuser;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+    Optional<AppUser> findByEmail(String email);
+}
